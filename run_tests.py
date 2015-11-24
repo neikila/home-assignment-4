9ed51bd26be82ba95e75f92fdde9cb992f92d8b5
Ivan Linnik <ivan_linnik@mail.ru>
2015-11-24 20:17:17 +0300

    Fix typo in run_tests

--- run_tests.py
+++ run_tests.py
@@ -8,7 +8,7 @@ from test.search.tests import *
 if __name__ == '__main__':
     suite = unittest.TestSuite((
         unittest.makeSuite(PositiveTests),
-        unittest.makeSuite(NegativeTests,)
+        unittest.makeSuite(NegativeTests),
     ))
     result = unittest.TextTestRunner().run(suite)
     sys.exit(not result.wasSuccessful())
