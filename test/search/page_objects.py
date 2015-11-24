2d42ec3cee6381118bcd310ddd8d16fbdb37c559
Kirill <neikila@gmail.com>
2015-11-24 19:44:28 +0300

    Temp fix
    Костыль номер раз

--- test/search/page_objects.py
+++ test/search/page_objects.py
@@ -24,12 +24,10 @@ class SideBarForm(Component):
     SUBCATEGORY = "//div[@class='current-category']//a[text()='%s']"
 
     def set_category(self, category_name):
-        WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, self.CATEGORY % category_name))
-        )
+        time.sleep(1)
         element = self.driver.find_element_by_xpath(self.CATEGORY % category_name)
-        print(element.get_attribute("href"))
         self.driver.get(element.get_attribute("href"))
+        time.sleep(1)
         # ActionChains(self.driver).move_to_element(element).click(element).perform()
 
     def set_subcategory(self, subcategory_name):
